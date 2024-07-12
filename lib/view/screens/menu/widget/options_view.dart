23e879f8b954dea111928250fe3a0ed0bd6b9578
Afaf997 <afizaya076@gmail.com>
2024-07-12 23:38:40 +0300

    menu screen

--- lib/view/screens/menu/widget/options_view.dart
+++ lib/view/screens/menu/widget/options_view.dart
@@ -45,24 +45,28 @@ class OptionsView extends StatelessWidget {
                 onTap: () => RouterHelper.getDashboardRoute('home'),
                 leading: Image.asset(Images.home, width: 20, height: 20, color: Theme.of(context).textTheme.bodyLarge!.color),
                 title: Text(getTranslated('home', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                
               ) : const SizedBox(),
 
               ListTile(
                 onTap: () => ResponsiveHelper.isMobilePhone() ? onTap!(2) : RouterHelper.getDashboardRoute('order'),
                 leading: Image.asset(Images.ordericon, width: 20, height: 20,),
                 title: Text(getTranslated('my_order', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ),
 
               ListTile(
                 onTap: () => RouterHelper.getNotificationRoute(),
                 leading: Image.asset(Images.notificationicon, width: 20, height: 20,),
                 title: Text(getTranslated('notification', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ),
 
               ListTile(
                 onTap: () => RouterHelper.getProfileRoute(),
                 leading: Image.asset(Images.profileicon, width: 20, height: 20, ),
                 title: Text(getTranslated('profile', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ),
          
               // ListTile(
@@ -74,48 +78,57 @@ class OptionsView extends StatelessWidget {
                 onTap: () => RouterHelper.getCouponRoute(),
                 leading: Image.asset(Images.walleticon, width: 20, height: 20, ),
                 title: Text(getTranslated('coupon', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ),
               ResponsiveHelper.isDesktop(context) ? ListTile(
                 onTap: () => RouterHelper.getNotificationRoute(),
                 leading: Image.asset(Images.coupenicon, width: 20, height: 20, ),
                 title: Text(getTranslated('notifications', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ) : const SizedBox(),
                ListTile(
                 onTap: () => RouterHelper.getAddressRoute(),
                 leading: Image.asset(Images.addressicon, width: 20, height: 20, ),
                 title: Text(getTranslated('address', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ),
               ListTile(
                 onTap: () => Navigator.push(context,MaterialPageRoute(builder: (context)=>const LanguagemodelScreen())),
                 leading: Image.asset(Images.languageicon, width: 20, height: 20, ),
                 title: Text(getTranslated('language', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ),
               ListTile(
                 onTap: () => RouterHelper.getSupportRoute(),
                 leading: SizedBox(width:20,height: 20,child: Image.asset(Images.helpicon,)),
                 title: Text(getTranslated('help_and_support', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ),
               ListTile(
                 onTap: () => RouterHelper.getPolicyRoute(),
                 leading: SizedBox(width:20,height: 20,child: Image.asset(Images.policyicon,)),
                 title: Text(getTranslated('privacy_policy', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ),
               ListTile(
                 onTap: () => RouterHelper.getTermsRoute(),
                 leading: SizedBox(width:20,height: 20,child: Image.asset(Images.termsicon,color: Theme.of(context).textTheme.bodyLarge?.color,)),
                 title: Text(getTranslated('terms_and_condition', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ),
 
               if(policyModel != null && policyModel.returnPage != null && policyModel.returnPage!.status!) ListTile(
                 onTap: () =>  RouterHelper.getReturnPolicyRoute(),
                 leading: Image.asset(Images.returnPolicy, width: 20, height: 20,),
                 title: Text(getTranslated('return_policy', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ),
 
               if(policyModel != null && policyModel.refundPage != null  && policyModel.refundPage!.status!) ListTile(
                 onTap: () => RouterHelper.getRefundPolicyRoute(),
                 leading: Image.asset(Images.refundPolicy, width: 20, height: 20, ),
                 title: Text(getTranslated('refund_policy', context)!, style: rubikMedium.copyWith(fontSize: Dimensions.fontSizeLarge)),
+                  trailing:const Icon(Icons.arrow_forward_ios_rounded, size: 14),
               ),
               if(policyModel != null && policyModel.cancellationPage != null  && policyModel.cancellationPage!.status!) ListTile(
                 onTap: () => RouterHelper.getCancellationPolicyRoute(),
